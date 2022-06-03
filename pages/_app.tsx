a5d2bf35262c7e91962769b7c6d58b7c86d5c808
ruslan <rus.magamedov.99@bk.ru>
2022-06-04 01:07:18 +0300

    wrap each component inside layout one

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
+import { Box } from "@mui/system";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Box sx={{ minHeight: "calc(100vh - 64px - 132px)" }}>
+        <Component {...pageProps} />
+      </Box>
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
