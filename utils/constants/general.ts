e99fc5854c8229c50611bfefa444eee7b28ae497
ruslan <rus.magamedov.99@bk.ru>
2022-06-14 11:02:28 +0300

    added general constants

---
+++ utils/constants/general.ts
@@ -0,0 +1,17 @@
+export const CATEGORIES = [
+  "business",
+  "entertainment",
+  "general",
+  "health",
+  "science",
+  "sports",
+  "technology",
+];
+
+export const NAV_LINKS = [
+  { name: "home", path: "/", id: 1 },
+  { name: "news", path: "/news", id: 2 },
+  { name: "sources", path: "/sources", id: 3 },
+];
+
+export const APP_NAME = "News App";
